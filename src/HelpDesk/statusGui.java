ec3b1f32d2172b680a18422226596c8c7cd93947
timurburkholz <timurburk@googlemail.com>
2017-06-09 11:37:00 +0200

    xyz

--- src/HelpDesk/statusGui.java
+++ src/HelpDesk/statusGui.java
@@ -8,6 +8,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
@@ -33,10 +34,10 @@ import java.awt.event.ActionEvent;
 
 public class statusGui {
 	static JFrame sGui=new JFrame();
-	private final JTextArea txtpn = new JTextArea();
 	private final JLabel lblNewLabel = new JLabel("Ticketstatus");
 	private final JButton btnZurck = new JButton("zur\u00FCck");
 	private final JTable table_1 = new JTable();
+	private final JButton btnZurck_1 = new JButton("zur\u00FCck");
 	
 public statusGui(){
 	init();
@@ -44,48 +45,27 @@ public statusGui(){
 public void init() {
 	sGui=new JFrame();
 	sGui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	sGui.setBounds(100, 100, 450, 450);
+	sGui.setBounds(100, 100, 450, 326);
 	sGui.getContentPane().setLayout(null);
-	txtpn.setBounds(23, 44, 167, 315);
-	
-	
-	
-	sGui.getContentPane().add(txtpn);
 	lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 14));
 	lblNewLabel.setBounds(89, 11, 101, 22);
+	sGui.add(table_1);
 	
 	sGui.getContentPane().add(lblNewLabel);
-	btnZurck.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent arg0) {
-			do_btnZurck_actionPerformed(arg0);
-		}
-	});
+
+		
+	
 	btnZurck.setBounds(335, 377, 89, 23);
 	
 	sGui.getContentPane().add(btnZurck);
-	table_1.setBounds(261, 32, 135, 197);
+	table_1.setBounds(89, 58, 241, 155);
 	
 	sGui.getContentPane().add(table_1);
 	
 
 	
-	String table="`db136045x2588076`.`Ticket`";
-	String columnid="`idTicket`";
-	String columnStatus="`Status`";
-	
-	try {
-		String[] id = Connection.conHP.getContentOfColumn(table, columnid);
-		String[] status = Connection.conHP.getContentOfColumn(table, columnStatus);
-		for (int i=0;i<id.length;i++) {
-			
-			txtpn.append("Ticket-ID:"+id[i].substring(0)+"        Status:"+status[i].substring(0)+"\n");
-	        
-			}
-		} 
-		catch (Exception e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
+
+	String columnStatus="`Status`";	
 	String table1="`db136045x2588076`.`Ticket`";
 	String column="`idTicket`";
 	
@@ -111,19 +91,31 @@ public void init() {
 	String header[]=new String[]{"Ticket-ID","Status"};
 	dtm.setColumnIdentifiers(header);
 	table_1.setModel(dtm);
+	
+
+	
+	btnZurck_1.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent arg0) {
+			do_btnZurck_1_actionPerformed(arg0);
+		}
+	});
+	btnZurck_1.setBounds(335, 253, 89, 23);
+	
+	sGui.getContentPane().add(btnZurck_1);
+	dtm.addRow(header);
 	for (int i = 0; i < ids.length; i++) {
         dtm.addRow(new Object[] { ids[i], stat[i]}
         );}
+	
+
+
         
 	
 	
 	
 	
-	
-	
-	
 	}
-	protected void do_btnZurck_actionPerformed(ActionEvent arg0) {
+	protected void do_btnZurck_1_actionPerformed(ActionEvent arg0) {
 		sGui.setVisible(false);
 		conGui.cGui.setVisible(true);
 	}
