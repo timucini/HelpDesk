5efbd5db3f660bfda81bb16c200b6c068ac68922
Matthias Cohn <matthias.cohn@googlemail.com>
2017-06-18 20:35:55 +0200

    Updated Core und UserGUI
    Fehlt nur noch absenden des Tickets und Laden der bestehenden Tickets

--- src/user/Core.java
+++ src/user/Core.java
@@ -4,40 +4,42 @@
 package user;
 
 import java.awt.EventQueue;
+import java.sql.SQLException;
+
 import sysInf.*;
 import java.util.ArrayList;
 
-import javax.swing.JFileChooser;
-
 import dbHelper.*;
-
 
 /**
  * @author Matthias Cohn (565998)
- *@version 2017-06-16
+ * @version 2017-06-16
  */
 public class Core {
-	static SysInf actSysInf=new SysInf();
-	static dbHelper.ConfigLoader dbConf=new ConfigLoader();
+	final static String DBConfFile="user";
+	static SysInf actSysInf = new SysInf();
+	static dbHelper.ConfigLoader dbConf = new ConfigLoader();
 	static dbHelper.DBCon dbCon = new DBCon();
-	static ArrayList<String> AL=new ArrayList<String>();
-	static String[] Arr1=null;
-	static String[][] Arr2=null;
-	protected static String[][] SysConf=new String[2][14];
+	static ArrayList<String> AL = new ArrayList<String>();
+	static String[] Arr1 = null;
+	static String[][] Arr2 = null;
+	protected static String[][] SysConf = new String[2][14];
+
 	/**
 	 * 
 	 */
 	public Core() {
-		// TODO Auto-generated constructor stub
+
+	
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		loadDBs();
-		
+		loadSysInf();
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -49,85 +51,106 @@ public class Core {
 			}
 		});
 	}
-	
-	public static void loadDBs(){
-		//https://www.java-forum.org/thema/aktuellen-pfad-der-anwendung-ermitteln.21044/
+
+	public static void loadDBs() {
+		// https://www.java-forum.org/thema/aktuellen-pfad-der-anwendung-ermitteln.21044/
 		String working_dir = System.getProperty("user.dir");
-		working_dir+="\\admin.cfg";  //preselect file
+		working_dir += "\\"+DBConfFile +".cfg"; // preselect file
 		try {
 			dbConf.setsFilePath(working_dir);
 		} catch (Exception e) {
 			// Wenn Datei nicht gefunden... Fileopener?!
 			e.printStackTrace();
-		}		
-		//System.out.println(working_dir);
-		dbConnect(true);
-		
-		
+		}
+		// System.out.println(working_dir);
+		// dbConnect(true);
 	}
 
-	public static boolean dbConnect(boolean bDoConnect){
-		try {
-			AL=dbConf.getsArrLConfigItems();
-			switch (AL.size()){
-			case 3:
-				dbCon.setConnection(AL.get(0),AL.get(1), AL.get(2));				
-			break;
-			case 4:
-				dbCon.setConnection(AL.get(0),AL.get(1), AL.get(2), AL.get(3));
-			break;
-			default:
-				throw new Exception ("Datenbankkonfiguration unvollständig.");
+	public static boolean dbConnect(boolean bDoConnect) {
+		if (bDoConnect == true) {
+			try {
+				AL = dbConf.getsArrLConfigItems();
+				switch (AL.size()) {
+				case 3:
+					dbCon.setConnection(AL.get(0), AL.get(1), AL.get(2));
+					break;
+				case 4:
+					dbCon.setConnection(AL.get(0), AL.get(1), AL.get(2), AL.get(3));
+					break;
+				default:
+					throw new Exception("Datenbankkonfiguration unvollständig.");
+				}
+				dbCon.connect();
+				System.out.println("Verbunden mit DB");
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("Datenbankverbindung nicht möglich");
+				dbConf.clearConfig(false);
+				return false;
 			}
-			dbCon.connect();
-			System.out.println("Verbunden mit DB");
-		} catch (Exception e) {
-			System.out.println("Datenbankverbindung nicht möglich");
-			dbConf.clearConfig(false);
-			return false;			
-		}				
+		} else {
+			try {
+				dbCon.disconnect();
+				System.out.println("Datenbankverbindung erfolgreich getrennt");
+			} catch (SQLException e) {
+				e.printStackTrace();
+				System.out.println("Trennen der Datenbankverbindung nicht möglich");
+				return true;
+			}
+		}
 		return dbCon.isConnected();
 	}
-	
-	public static boolean isDbConnected(){
+
+	public static boolean isDbConnected() {
 		return dbCon.isConnected();
 	}
-	
-	private static void loadSysInf(){
-		SysConf[0][0]="Computername";
-		SysConf[1][0]=actSysInf.getComputername();
-		SysConf[0][1]="Anmeldserver";
-		SysConf[1][1]=actSysInf.getLogonServer();
-		SysConf[0][2]="Benutzername";
-		SysConf[1][2]=actSysInf.getUserName();
-		SysConf[0][3]="Betriebssystem";
-		SysConf[1][3]=actSysInf.getOS();
-		SysConf[0][4]="CPU";
-		SysConf[1][4]=actSysInf.getProcessor();
-		SysConf[0][5]="Anz. d. CPUs";
-		SysConf[1][5]=String.valueOf(actSysInf.getNumberOfCPU());
-		SysConf[0][6]="Systemlaufwerk";
-		SysConf[1][6]=actSysInf.getSysDrive();
-		SysConf[0][7]="Kapazität Systempartition";
-		SysConf[1][7]=Double.toString(actSysInf.getSysDriveCap());
-		SysConf[0][8]="Freier Speicherplatz Sys";
-		SysConf[1][8]=Double.toString(actSysInf.getSysDriveFree());
-		SysConf[0][9]="Nutzung Systempartition";
-		SysConf[1][9]=String.valueOf(actSysInf.getSysDriveUsage())+"%";
-		SysConf[0][10]="RAM";
-		SysConf[1][10]="RAM kann nicht ausgelesen werden";
-		SysConf[0][11]="IPv4-Addresse";
-		SysConf[1][11]=actSysInf.getIPv4Addr();
-		SysConf[0][12]="IPv6-Addresse";
-		SysConf[1][12]=actSysInf.getIPv6Addr();
-		SysConf[0][13]="MAC";
-		SysConf[1][13]=actSysInf.getMAC();	
+
+	private static void loadSysInf() {
+		SysConf[0][0] = "Computername";
+		SysConf[1][0] = actSysInf.getComputername();
+		SysConf[0][1] = "Anmeldserver";
+		SysConf[1][1] = actSysInf.getLogonServer();
+		SysConf[0][2] = "Benutzername";
+		SysConf[1][2] = actSysInf.getUserName();
+		SysConf[0][3] = "Betriebssystem";
+		SysConf[1][3] = actSysInf.getOS();
+		SysConf[0][4] = "CPU";
+		SysConf[1][4] = actSysInf.getProcessor();
+		SysConf[0][5] = "Anz. d. CPUs";
+		SysConf[1][5] = String.valueOf(actSysInf.getNumberOfCPU());
+		SysConf[0][6] = "Systemlaufwerk";
+		SysConf[1][6] = actSysInf.getSysDrive();
+		SysConf[0][7] = "Kapazität Systempartition";
+		SysConf[1][7] = Double.toString(actSysInf.getSysDriveCap());
+		SysConf[0][8] = "Freier Speicherplatz Sys";
+		SysConf[1][8] = Double.toString(actSysInf.getSysDriveFree());
+		SysConf[0][9] = "Nutzung Systempartition";
+		SysConf[1][9] = String.valueOf(actSysInf.getSysDriveUsage()) + "%";
+		SysConf[0][10] = "RAM";
+		SysConf[1][10] = "RAM kann nicht ausgelesen werden";
+		SysConf[0][11] = "IPv4-Addresse";
+		SysConf[1][11] = actSysInf.getIPv4Addr();
+		SysConf[0][12] = "IPv6-Addresse";
+		SysConf[1][12] = actSysInf.getIPv6Addr();
+		SysConf[0][13] = "MAC";
+		SysConf[1][13] = actSysInf.getMAC();
 	}
-	
-	public static String[][] getSysInf(){
+
+	public static String[][] getSysInf() {
 		loadSysInf();
 		return SysConf;
 	}
 	
+	public static ArrayList<String> arrLDropDown(String Table, String Col){
+		ArrayList<String> ArrL=new ArrayList<String>();
+		try {
+			ArrL=dbCon.getContentOfColumnList(Table, Col);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}		
+		return ArrL;
+	}
 
 }
--- src/user/UserGUI.java
+++ src/user/UserGUI.java
@@ -14,6 +14,8 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+
 import javax.swing.ListSelectionModel;
 import javax.swing.JScrollPane;
 import javax.swing.border.TitledBorder;
@@ -23,6 +25,11 @@ import javax.swing.JTabbedPane;
 import javax.swing.JLayeredPane;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.SwingConstants;
+import javax.swing.JComboBox;
+import javax.swing.JTextPane;
 
 public class UserGUI extends JFrame {
 
@@ -35,14 +42,34 @@ public class UserGUI extends JFrame {
 	private static JLabel lbl_isConnected = new JLabel("Nicht verbunden");
 	private static final JScrollPane scrollP_SysInf = new JScrollPane();
 	private final JMenuBar menuBar = new JMenuBar();
-	private static final JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-	private static final JLayeredPane layeredPane = new JLayeredPane();
-	private static final JLayeredPane layeredPane_1 = new JLayeredPane();
 	private final JMenu mnDatei = new JMenu("Datei");
 	private final JMenu mnHilfe = new JMenu("Hilfe");
 	private final JMenuItem mntmber = new JMenuItem("Über");
-	private final JMenuItem mntmDatenbankVerbinden = new JMenuItem("Datenbank verbinden");
 	private final JMenuItem mntmBeenden = new JMenuItem("Beenden");
+	private final JMenu menu = new JMenu("Datenbank");
+	private final JMenuItem mntmDBVerbinden = new JMenuItem("verbinden");
+	private final JMenuItem mntmDBTrennen = new JMenuItem("trennen");
+	private static final JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+	private static final JPanel panel_Status = new JPanel();
+	private static final JTabbedPane tabbedPane_NewTicket = new JTabbedPane(JTabbedPane.TOP);
+	private static final JPanel panel_direct = new JPanel();
+	private static final JLabel lbl_Info = new JLabel();
+	private static final JPanel panel_Contact = new JPanel();
+	private static JTextField txt_Name = new JTextField();
+	private static JTextField txt_phone = new JTextField();
+	private static JTextField txt_Mail = new JTextField();
+	private static JPanel panel_Issue = new JPanel();
+	private static JPanel panel_location = new JPanel();
+	private static JTextField txt_Geb = new JTextField();
+	private static final JPanel panel_send = new JPanel();
+	private static JButton cmd_send = new JButton("absenden");
+	private final JLabel lbl_Info2 = new JLabel("<html><Body>Bitte überprüfen Sie Ihre vorherigen Eingaben,<br> bevor Sie auf 'absenden' klicken <br><b>-</b><br>Das erspart ggf. Rückfragen</Body></HTML>");
+	private static JComboBox<String> cbB_HW = new JComboBox<String>();
+	private static JComboBox<String> cbB_SW = new JComboBox<String>();
+	private static JScrollPane scrollPane = new JScrollPane();
+	private static JTextPane txtPane_Issue = new JTextPane();
+	private static JTextField txt_Raum = new JTextField();
+	private static JTextField txt_Anschluss = new JTextField();
 
 	/**
 	 * Create the frame.
@@ -56,33 +83,139 @@ public class UserGUI extends JFrame {
 		});
 		setTitle("HelpDesk - User-Interface");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 500);
+		setBounds(100, 100, 480, 450);
 
 		setJMenuBar(menuBar);
 
 		menuBar.add(mnDatei);
-		mntmDatenbankVerbinden.addActionListener(new ActionListener() {
+		
+		mnDatei.add(menu);
+		mntmDBVerbinden.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				do_mntmDatenbankVerbinden_actionPerformed(arg0);
+				do_mntmDBVerbinden_actionPerformed(arg0);
 			}
 		});
-
-		mnDatei.add(mntmDatenbankVerbinden);
+		
+		menu.add(mntmDBVerbinden);
+		mntmDBTrennen.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				do_mntmDBTrennen_actionPerformed(e);
+			}
+		});
+		
+		menu.add(mntmDBTrennen);
+		mntmBeenden.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				do_mntmBeenden_actionPerformed(e);
+			}
+		});
 
 		mnDatei.add(mntmBeenden);
 
 		menuBar.add(mnHilfe);
 
 		mnHilfe.add(mntmber);
+		menuBar.add(lbl_isConnected);
+		lbl_isConnected.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lbl_isConnected.setForeground(Color.RED);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		lbl_isConnected.setBounds(220, 15, 92, 14);
-		lbl_isConnected.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lbl_isConnected.setForeground(Color.RED);
+		
+		
+		
+		tabbedPane.setBounds(0, 0, 464, 390);
+		
+		contentPane.add(tabbedPane);
+		
+		tabbedPane.addTab("Ticket Status", null, panel_Status, null);
+		
+		tabbedPane.addTab("Neues Ticket anlegen", null, tabbedPane_NewTicket, null);
+		
+		tabbedPane_NewTicket.addTab("1. Ansprechpartner", null, panel_Contact, "Wer meldet die Störung / Wie erreichen Wir Sie");
+		panel_Contact.setLayout(null);
+		txt_Name.setBounds(10, 11, 404, 39);
+		txt_Name.setColumns(10);
+		txt_Name.setBorder(new TitledBorder(null, "Name Meldender / Ansprechpartner", TitledBorder.LEADING,
+						TitledBorder.TOP, null, null));
+		
+		panel_Contact.add(txt_Name);
+		txt_phone.setBounds(10, 61, 404, 39);
+		txt_phone.setColumns(10);
+		txt_phone.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Telefon Ansprechpartner",
+						TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		
+		panel_Contact.add(txt_phone);
+		txt_Mail.setBounds(10, 111, 404, 39);
+		txt_Mail.setColumns(10);
+		txt_Mail.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "E-Mail Ansprechpartner",
+						TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		
+		panel_Contact.add(txt_Mail);
+		tabbedPane_NewTicket.addTab("2. Fehlerbeschreibung", null, panel_Issue, "Beschreiben Sie den Fehler");
+		panel_Issue.setLayout(null);
+		cbB_HW.setBorder(new TitledBorder(null, "An welchem Ger\u00E4t tritt der Fehler auf:", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		cbB_HW.setBounds(10, 11, 434, 40);
+		
+		panel_Issue.add(cbB_HW);
+		cbB_SW.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Mit welcher Software der Fehler auf:", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		cbB_SW.setBounds(10, 62, 434, 40);
+		
+		panel_Issue.add(cbB_SW);
+		scrollPane.setBorder(new TitledBorder(null, "Beschreiben Sie den Fehler genauer", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		scrollPane.setBounds(10, 113, 434, 160);
+		
+		panel_Issue.add(scrollPane);
+		
+		scrollPane.setViewportView(txtPane_Issue);
+		
+		tabbedPane_NewTicket.addTab("3. Lokation", null, panel_location, "Wo tritt der Fehler auf / Wo erreichen Wir Sie");
+		panel_location.setLayout(null);
+		txt_Geb.setBounds(10, 11, 434, 39);
+		txt_Geb.setColumns(10);
+		txt_Geb.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Adresse / Geb\u00E4ude", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		
+		panel_location.add(txt_Geb);
+		txt_Raum.setColumns(10);
+		txt_Raum.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Raum", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		txt_Raum.setBounds(10, 61, 434, 39);
+		
+		panel_location.add(txt_Raum);
+		txt_Anschluss.setColumns(10);
+		txt_Anschluss.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Anschluss (nur wenn bekannt)", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		txt_Anschluss.setBounds(10, 111, 434, 39);
+		
+		panel_location.add(txt_Anschluss);
+		
+		tabbedPane_NewTicket.addTab("4. Senden", null, panel_send, "Bitte überprüfen Sie Ihre vorherigen Eingaben, bevor Sie auf 'absenden' klicken - Das erspart ggf. Rückfragen");
+		panel_send.setLayout(null);
+		cmd_send.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				do_cmd_send_actionPerformed(e);
+			}
+		});
+		cmd_send.setBounds(172, 192, 100, 23);
+		
+		panel_send.add(cmd_send);
+		lbl_Info2.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_Info2.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lbl_Info2.setBounds(10, 71, 404, 57);
+		
+		panel_send.add(lbl_Info2);
+		tabbedPane.addTab("Direktkontakt zum Helpdesk", null, panel_direct, null);
+		panel_direct.setLayout(null);
+		lbl_Info.setText("<html><body>Sie erreichen uns unter der Telefonnummer:<br>"+
+				"<b>0800 - 123456789</b><br>"+
+				"Bitte halten Sie die unten aufgeführte Systemkonfiguration bereit.</body></html>");
 
-		contentPane.add(lbl_isConnected);
+		lbl_Info.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lbl_Info.setBorder(new TitledBorder(null, "Info", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		lbl_Info.setBounds(10, 11, 409, 88);
+		
+		panel_direct.add(lbl_Info);
+		scrollP_SysInf.setBounds(10, 110, 409, 241);
+		panel_direct.add(scrollP_SysInf);
 
 		/*
 		 * modTable(); getTable();
@@ -91,13 +224,31 @@ public class UserGUI extends JFrame {
 		updatelbl(Core.isDbConnected());
 	}
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	private static void showSysInf() {
 		Core.getSysInf();
+
 		scrollP_SysInf.setBorder(new TitledBorder(null, "Aktuelle Systemkonfiguration", TitledBorder.LEADING,
 				TitledBorder.TOP, null, null));
-		scrollP_SysInf.setBounds(10, 327, 414, 102);
-
-		contentPane.add(scrollP_SysInf);
 		tm_SysInf = new JTable(Core.SysConf[0].length, Core.SysConf.length);
 		scrollP_SysInf.setViewportView(tm_SysInf);
 		tm_SysInf.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
@@ -108,14 +259,6 @@ public class UserGUI extends JFrame {
 		// https://www.java-forum.org/thema/jtable-spaltennamen-aendern.2266/
 		tm_SysInf.setModel(new DefaultTableModel(new Object[Core.SysConf[0].length][Core.SysConf.length],
 				new String[] { "Eigenschaft", "Wert" }));
-		tabbedPane.setBounds(10, 45, 414, 271);
-
-		contentPane.add(tabbedPane);
-
-		tabbedPane.addTab("Ticket Status", null, layeredPane, null);
-
-		tabbedPane.addTab("Neues Ticket anlegen", null, layeredPane_1, null);
-
 		modTable(Core.SysConf); // Tabelle füllen
 	}
 
@@ -145,13 +288,49 @@ public class UserGUI extends JFrame {
 			lbl_isConnected.setText("Nicht verbunden");
 		}
 	}
+	
+	protected void do_mntmDBVerbinden_actionPerformed(ActionEvent arg0) {
+		updatelbl(Core.dbConnect(true));
+		fillcbBoxes(Core.isDbConnected());
+		// System.out.println(Core.isDbConnected());
+	}
+
+	protected void do_mntmDBTrennen_actionPerformed(ActionEvent e) {
+		updatelbl(Core.dbConnect(false));
+		fillcbBoxes(Core.isDbConnected());
+	}
 
 	protected void do_this_windowClosing(WindowEvent arg0) {
 		System.exit(0);
 	}
 
-	protected void do_mntmDatenbankVerbinden_actionPerformed(ActionEvent arg0) {
-		updatelbl(Core.dbConnect(true));
-		System.out.println(Core.isDbConnected());
+	protected void do_mntmBeenden_actionPerformed(ActionEvent arg0) {
+		System.exit(0);
 	}
+
+	protected void do_cmd_send_actionPerformed(ActionEvent e) {
+
+	}
+	
+	private void fillcbBoxes(boolean load){
+		if (load){
+			ArrayList<String> ArrLHW=Core.arrLDropDown("HW", "HWType");
+			ArrayList<String> ArrLSW=Core.arrLDropDown("SW", "SWType");	
+			if (ArrLHW!=null && ArrLSW!=null){
+				for (String s:ArrLHW){
+					cbB_HW.addItem(s);
+				}
+				for (String s:ArrLSW){
+					cbB_SW.addItem(s);
+				}
+			}else fillcbBoxes(false);
+		}
+		else{
+			cbB_HW.removeAllItems();
+			cbB_SW.removeAllItems();
+		}
+	}
+	
+	// TODO: Checker, ob die Richtige DB/Tabellen/Spalten vorhanden sind
+	
 }
